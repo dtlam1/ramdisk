1121311fcbfd564161ee4b03eb7f354f730ff002
root <root@DTLam1.DE1>
2022-10-24 10:23:31 +0700

    UPDATE excute file

--- createRamDisk.sh
+++ createRamDisk.sh
@@ -1,10 +1,17 @@
+TOKEN=$1
 DISK_DIR=/ramdisk-tmp
 EXEC_DIR=/srv/ramdisk
+WORKING_REPO=scg-id-nuxt-frontend
 
 # create ram dir
 sudo mkdir $DISK_DIR
-sudo chmod -R 0755 $DISK_DIR
+sudo chmod -R 0777 $DISK_DIR
 sudo mount -t tmpfs -o rw,size=2G tmpfs $DISK_DIR
+
+cd $DISK_DIR
+git clone git clone https://$TOKEN@github.com/scg-wedo/$WORKING_REPO
+cd $WORKING_REPO
+npm i
 
 # create backup dir
 
