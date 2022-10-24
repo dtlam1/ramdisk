0aeeeffe356ee2cbaacd6cdbf58aceefcf84f381
root <root@DTLam1.DE1>
2022-10-24 13:29:29 +0700

    UPDATE .

--- createRamDisk.sh
+++ createRamDisk.sh
@@ -8,11 +8,11 @@ sudo mkdir $DISK_DIR
 sudo mkdir /ramdisk-backup
 sudo chmod -R 0777 $DISK_DIR
 sudo mount -t tmpfs -o rw,size=2G tmpfs $DISK_DIR
-
+echo $TOKEN
 cd $DISK_DIR
-git clone git clone https://$TOKEN@github.com/scg-wedo/$WORKING_REPO
+git clone https://$TOKEN@github.com/scg-wedo/$WORKING_REPO
 cd $WORKING_REPO
 npm i
 
 # cron backup
-*/5 * * * * sh $EXEC_DIR/bakckup.sh
+echo "*/5 * * * * sh $EXEC_DIR/bakckup.sh" | crontab -
